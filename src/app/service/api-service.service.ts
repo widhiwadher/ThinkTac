5520eccf7d8cf1a16b1444b4a8cc72a74b1a9bca
widhiwadher <76730927+widhiwadher@users.noreply.github.com>
2022-05-13 14:40:54 +0530

    Add files via upload

--- src/app/service/api-service.service.ts
+++ src/app/service/api-service.service.ts
@@ -9,18 +9,19 @@ export class ApiServiceService {
     'https://gist.githubusercontent.com/manzooralam/4b6aca6e1aa794c57c0cb6e7a2605109/raw/694828a0b1084ef63ff507ed85de3db6e3931e99/list.json';
   filterValue: any = '';
   dataList: any = '';
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
 
   callingService() {
-    this._http.get(this.baseUrl).subscribe(res => {
-      this.dataList = res
+    this._http.get(this.baseUrl).subscribe((res) => {
+      this.dataList = res;
     });
     return this._http.get(this.baseUrl);
   }
 
   filterData(str: any) {
     this.filterValue = this.dataList.filter((item: any) => {
-      return item.Name.includes(str);
+      console.log(item.Name.toLowerCase());
+      return item.Name.toLowerCase().includes(str.toLowerCase());
     });
     return this.filterValue;
   }
